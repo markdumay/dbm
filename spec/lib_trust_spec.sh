1de0f1788f7ca0da4ad72636a031548da2c91c60
mark <mark@dumay.nl>
2021-04-06 20:03:53 +0200

    Fixed trust unit tests

--- spec/lib_trust_spec.sh
+++ spec/lib_trust_spec.sh
@@ -42,7 +42,7 @@ Describe 'lib/trust.sh' trust
         It 'derives the username of a key file'
             When call _get_username_from_key "$1"
             The status should be "$4"
-            The output should equal "$2"
+            The output should start with "$2"
             The error should start with "$3"
         End
     End
@@ -50,53 +50,57 @@ Describe 'lib/trust.sh' trust
 
     # Todo 'add_repository_signer()'
 
-    Describe 'generate_delegate_key()'
-        setup_local() {
+    Describe 'generate_delegate_key()' test
+        setup_local_delegate() {
             dummy_file=$(mktemp -t "delegate-test.XXXXXXXXX")
+            dummy_file=$(echo "${dummy_file}" | sed 's|/delegate-test.XXXXXXXXX.|/delegate-test.|g') # macOS/mktemp fix
             path=$(dirname "${dummy_file}")
         }
 
-        cleanup_local() {
-            [ -f "${dummy_file}" ] && rm -rf "${dummy_file}"
+        cleanup_local_delegate() {
+            { [ -f "${path}/delegate-test.crt" ] && rm -rf "${path}/delegate-test.crt"; } || true
+            { [ -f "${path}/delegate-test.csr" ] && rm -rf "${path}/delegate-test.csr"; } || true
+            { [ -f "${path}/delegate-test.key" ] && rm -rf "${path}/delegate-test.key"; } || true
         }
         
-        BeforeAll 'setup_local'
-        AfterAll 'cleanup_local'
+        BeforeAll 'setup_local_delegate'
+        AfterAll 'cleanup_local_delegate'
 
         Parameters
-            'delegate-test' '/O=DBM Trust Test' "${path}" '*Signature ok*Getting Private key*' success
+            'delegate-test' '' "${path}" '/O=DBM Trust Test' '*Signature ok*Getting Private key*' 'Generated passphrase:*' success
         End
 
         It 'generates a delegate key'
-            When call generate_delegate_key "$1" "$2" "$3"
-            The status should be "$5"
-            The error should match pattern "$4"
+            When call generate_delegate_key "$1" "$2" "$3" "$4"
+            The status should be "$7"
+            The error should match pattern "$5"
+            The output should match pattern "$6"
+            Dump
         End
     End
 
-    Describe 'generate_signer_key()' test
-        setup_local() {
+    Describe 'generate_signer_key()'
+        setup_local_signer() {
             dummy_file=$(mktemp -t "signer-test.XXXXXXXXX")
+            dummy_file=$(echo "${dummy_file}" | sed 's|/signer-test.XXXXXXXXX.|/signer-test.|g') # macOS/mktemp fix
             path=$(dirname "${dummy_file}")
         }
 
-        # TODO: clean private key
-        cleanup_local() {
-            [ -f "${dummy_file}" ] && rm -rf "${dummy_file}"
+        cleanup_local_signer() {
+            { [ -f "${path}/signer-test.pub" ] && rm -rf "${path}/signer-test.pub"; } || true
         }
         
-        BeforeAll 'setup_local'
-        AfterAll 'cleanup_local'
+        BeforeAll 'setup_local_signer'
+        AfterAll 'cleanup_local_signer'
 
         Parameters
-            'signer-test' "${path}" success
+            'signer-test' '' "${path}" success
         End
 
         It 'generates a signer key'
-            When call generate_signer_key "$1" "$2"
-            The status should be "$3"
-            # The error should match pattern "$4"
-            Dump
+            When call generate_signer_key "$1" "$2" "$3"
+            The status should be "$4"
+            The output should match pattern "*Corresponding public key available: *signer-test.pub*"
         End
     End
 
