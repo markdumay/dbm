67a2daa462eb5cd1ab224ca889dbc191906a6e4c
mark <mark@dumay.nl>
2021-04-08 18:06:39 +0200

    Adjusted read_dependencies()

--- lib/config.sh
+++ lib/config.sh
@@ -525,7 +525,7 @@ read_dependencies() {
 
     # retrieve all dependendencies from the DBM config file with their line number
     # remove all comments, trailing spaces, and protocols; separate each dependency by a ';'
-    dependencies=$(grep -n '^DBM_.*VERSION=.*' "${config_file}" | sed -r 's/(^[0-9]*:)DBM_/\1/g;')
+    dependencies=$(grep -En '^DBM_.*VERSION=.*' "${config_file}" | sed -r 's/(^[0-9]*:)DBM_/\1/g;')
     dependencies=$(echo "${dependencies}" | sed 's/hub.docker.com\/r\//hub.docker.com\//g')
     dependencies=$(echo "${dependencies}" | sed 's/_VERSION=/ /g;s/\// /g;')
     dependencies=$(echo "${dependencies}" | sed -e 's/\s*#.*$//;s/[[:space:]]*$//;')
